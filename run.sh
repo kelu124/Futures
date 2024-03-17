f8e10d8507eb803e32119e4eb2ca6c848fc209bd
kelu124 <kelu124@gmail.com>
2024-03-17 10:52:30 +0100

    cleanup

--- run.sh
+++
@@ -1,7 +0,0 @@
-jupyter nbconvert --to notebook --execute 00.getURLs.ipynb
-jupyter nbconvert --to notebook --execute 01.Process.ipynb
-jupyter nbconvert --to notebook --execute 02.Cleaning.ipynb 
-jupyter nbconvert --to notebook --execute 03.GetSeeds.ipynb
-jupyter nbconvert --to notebook --execute 05.ReadSeeds.ipynb
-jupyter nbconvert --to notebook --execute 06.CreatePages.ipynb
-rm *nbconvert* 
